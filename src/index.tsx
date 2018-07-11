c16e96239a1f68bc31c47a7c2775fad66f8aac9d
rtheone <rtheone@gmail.com>
2018-07-11 14:28:24 -0700

    Copied from previous repo. Updated dependencies.

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import 'normalize.css';
+
+import React from 'react';
+import { render } from 'react-dom';
+import { createRenderer } from 'fela';
+import { createComponent, Provider } from 'react-fela';
+const renderer = createRenderer();
+
+import sum from './sum.ts';
+
+const Button = createComponent(
+  props => css`
+    background: ${props.color};
+    color: white;
+  `,
+  'button'
+);
+
+const AlternateButton = createComponent(
+  props => css`
+    background: orangered;
+  `,
+  Button
+);
+
+const App = () => (
+  <div>
+    <h1>Here's a text element.</h1>
+    <Button color="steelblue">1 + 2 =</Button>
+    <AlternateButton>{sum(1, 2)}</AlternateButton>
+  </div>
+);
+
+render(
+  <Provider renderer={renderer}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
